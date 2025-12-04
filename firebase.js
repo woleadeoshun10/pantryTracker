c262e27d2bd6d72b14e6ec0753575637a1738ae6
Adewole <woleadeoshun18@gmail.com>
2025-12-04 14:02:54 -0500

    Project set up + gitgnore

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; // Correct import
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCPI3cXVpPFJjqFeqF0TmVHd3tXSOtCpyk",
+  authDomain: "hspanntryapp.firebaseapp.com",
+  projectId: "hspanntryapp",
+  storageBucket: "hspanntryapp.appspot.com",
+  messagingSenderId: "515744385790",
+  appId: "1:515744385790:web:2e8cc2f7ac839a366273af",
+  measurementId: "G-4WRQT04ZV1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app); // Correct usage
+
+export { app, firestore };
+
